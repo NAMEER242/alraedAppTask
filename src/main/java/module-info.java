90d5b8762e38b816485a92ff2661b9338f2d12a6
NAMEER242 <nmeer5242@gmail.com>
2022-03-15 13:47:31 +0300

    adding request jdk.crypto.ec and request jdk.crypto.cryptoki to the module-info file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module n242.alraed.appTask {
     requires javafx.fxml;
     requires java.xml;
     requires transitive java.desktop;
+    requires jdk.crypto.ec;
+    requires jdk.crypto.cryptoki;
 
     opens n242.alraed.appTask to javafx.fxml;
     exports n242.alraed.appTask;
